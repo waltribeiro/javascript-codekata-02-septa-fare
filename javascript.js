ff566695251fe3591dd5cbd6c9c9edd65e54b970
WaltRibeiro <walt@fororchestra.com>
2019-12-24 12:16:43 -0500

    commit

--- javascript.js
+++ javascript.js
@@ -1,13 +1,8 @@
-<button>Click Me!</button>
-var waltee = "hiya"
-
-document.write(`I love ${waltee}`);
+prompt("this is your prompt")
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+
   
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
