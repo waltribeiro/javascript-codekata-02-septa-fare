677afb995e81204fcd847b23cd810eda4a948561
WaltRibeiro <walt@fororchestra.com>
2019-12-30 20:02:11 -0500

    commit

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 
-  
+  var price = 24.00
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
